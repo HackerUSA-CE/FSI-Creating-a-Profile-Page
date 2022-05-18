0ca12c31840417829a0100b63c577e5893f0aa7f
YSC <technosayge@gmail.com>
2022-05-17 23:21:00 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+
 let content = document.querySelector('.js-generated.content')
+
+let header = document.createElement('h1')
+header.setAttribute('class', 'dog-name')
+header.append('Rizzo')
+content.append(header)
+
+let dogContent = document.createElement('div')
+dogContent.setAttribute('class', 'dog-content')
+content.append(dogContent)
+
+let dogImage = document.createElement('img')
+dogImage.setAttribute('class', 'dog-image')
+dogImage.setAttribute('src', './assets/rizzo.jpg')
+content.append(dogImage)
+
+let dogDetails = document.createElement('div')
+dogDetails.setAttribute('class', 'dog-details')
+content.append(dogDetails)
+
+
